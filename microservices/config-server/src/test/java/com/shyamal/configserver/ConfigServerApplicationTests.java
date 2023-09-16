520a76beabf7e9c652794dc229b3519b28d2ebda
git1st <shyamalgit1@gmail.com>
2023-09-16 21:43:54 +0600

    mocroservices

---
+++ microservices/config-server/src/test/java/com/shyamal/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyamal.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
